c0a8dc7e81af88ab9a02b7a6bc4064bf89b3faa5
CovenantJunior <covenanttope@gmail.com>
2024-11-11 05:27:38 +0100

    controllers

--- lib/layouts/qr_generator.dart
+++ lib/layouts/qr_generator.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
 class QRGenerator extends StatefulWidget {
   const QRGenerator({super.key});
@@ -13,6 +17,51 @@ class _QRGeneratorState extends State<QRGenerator> {
   Widget build(BuildContext context) {
     TextEditingController? textEditingController;
     ScreenshotController? screenshotController;
-    return const Scaffold();
+    String data = "";
+    String selectedTyped = "Text";
+    final Map<String, TextEditingController> controllers = {
+      "name" : TextEditingController(),
+      "phone": TextEditingController(),
+      "email" : TextEditingController(),
+      "url" : TextEditingController()
+    };
+
+    String generatedData() {
+      switch (selectedTyped) {
+        case 'contact':
+          return """BEGIN:VCARD
+            VERSION:3.0
+            FN" : ${controllers['name']?.text},
+            TEL": ${controllers['phone']?.text},
+            EMAIL" : ${controllers['email']?.text},
+            END:VCARD
+          """;
+
+          case 'url':
+          String url = controllers['url']!.text;
+          if(!url.startsWith('http://') && !url.startsWith('https://')) {
+            url = "https://${url}";
+          }
+          return url;
+          
+        default:
+        return textEditingController!.text;
+      }
+    }
+
+
+    void shareQR() async {
+      final directory = await getApplicationDocumentsDirectory();
+      final imagePath = "${directory.path}/qr.png";
+      final capture = screenshotController!.capture();
+
+      File imageFile = File(imagePath);
+      await imageFile.writeAsBytes(capture as List<int>);
+      await Share.shareXFiles([XFile(imagePath)], text: "QR Code");
+    }
+    
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
