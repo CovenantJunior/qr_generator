c0f0fc5fa38cd42fe4a207caee8389762ae41753
CovenantJunior <covenanttope@gmail.com>
2024-11-18 13:54:04 +0100

    widget build for qr generator

--- lib/layouts/qr_generator.dart
+++ lib/layouts/qr_generator.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:share_plus/share_plus.dart';
@@ -60,12 +61,51 @@ class _QRGeneratorState extends State<QRGenerator> {
       await Share.shareXFiles([XFile(imagePath)], text: "QR Code");
     }
 
-    Widget buildText(context, label) {
-      return const Text('data');
+    Widget buildText(textEditingController, label) {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: textEditingController,
+          onChanged: (e) => {
+            data = generatedData()
+          },
+        ),
+      );
+    }
+
+    Widget inputFields() {
+      switch (selectedTyped) {
+        case 'contact':
+          return Column(
+            children: [
+              buildText(controllers['name'], 'Name'),
+              buildText(controllers['phone'], 'Phone'),
+              buildText(controllers['email'], 'Email'),
+            ],
+          );
+        
+        case 'url':
+          return buildText(controllers['url'], 'URL');
+        default: return const SizedBox();
+      }
     }
     
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Generate QR",
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+      body: buildText(textEditingController, 'Hey'),
     );
   }
 }
